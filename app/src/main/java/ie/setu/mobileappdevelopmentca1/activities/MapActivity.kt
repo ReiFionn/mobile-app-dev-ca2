b4eb530245e86ec65e4572b75e047593b9b25a31
Fionn Reilly <20101977@mail.wit.ie>
2025-12-12 15:55:55 +0000

    Tidy up generated code

--- app/src/main/java/ie/setu/mobileappdevelopmentca1/activities/MapActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevelopmentca1/activities/MapActivity.kt
@@ -6,17 +6,16 @@ import android.os.Bundle
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
-import ie.setu.mobileappdevelopmentca1.R
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import ie.setu.mobileappdevelopmentca1.databinding.ActivityMapBinding
+import ie.setu.mobileappdevelopmentca1.R
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -29,21 +28,10 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val setu = LatLng(52.245696, -7.139102)
+        mMap.addMarker(MarkerOptions().position(setu).title("Marker in Waterford"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(setu, 16f))
     }
 }
