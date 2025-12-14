788d690f027b55e62b08f3b2d3745e01d0b94ba6
Fionn Reilly <20101977@mail.wit.ie>
2025-12-14 15:24:11 +0000

    simplify event maps activity

--- app/src/main/java/ie/setu/mobileappdevelopmentca1/activities/EventMapsActivity.kt
+++ app/src/main/java/ie/setu/mobileappdevelopmentca1/activities/EventMapsActivity.kt
@@ -1,18 +1,11 @@
 package ie.setu.mobileappdevelopmentca1.activities
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import ie.setu.mobileappdevelopmentca1.R
 import ie.setu.mobileappdevelopmentca1.databinding.ActivityEventMapsBinding
 
-class EventMapsActivity : AppCompatActivity() {
+class PlacemarkMapsActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityEventMapsBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,21 +15,5 @@ class EventMapsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
-
-        val navController = findNavController(R.id.nav_host_fragment_content_event_maps)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment_content_event_maps)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
     }
 }
